cfe7e13faf395b600addd4172cfd082911ac05db
Developer <jjstrain68@gmail.com>
2021-04-02 17:24:17 +0100

    Assignment - Mockito verify for VetSDJpaServiceTest

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    private VetRepository vetRepository;
+
+    @InjectMocks
+    private VetSDJpaService vetService;
+
+    @Test
+    void deleteById() {
+        vetService.deleteById(1l);
+
+        verify(vetRepository, times(1)).deleteById(1l);
+    }
+}
